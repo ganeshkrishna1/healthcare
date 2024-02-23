d42b206bb02c859c50345d70d912969002af2d8f
Mohankrish007 <mohangarnepudi2001@gmail.com>
2024-02-23 10:28:01 +0530

    login added

--- healthcare/src/pages/Login/Login.jsx
+++ healthcare/src/pages/Login/Login.jsx
@@ -1,6 +1,32 @@
 import React from "react";
 
 import "./Login.css";
+import Button from "../../components/ui/Button";
 export default function Login() {
-  return <div>Login</div>;
+  const handleOnClick = () => {
+    console.log("login");
+  };
+
+  return (
+    <div className="d-flex justify-content-center ">
+      <div className="login-details">
+        <h3 className="login-header d-flex justify-content-center p-2">
+          Login
+        </h3>
+        <div className="d-flex flex-column g-1 px-2 login-inputs">
+          <div>Email</div>
+          <input type="text" />
+        </div>
+        <div className="d-flex flex-column g-1 px-2 mb-3 login-inputs">
+          <div>Password</div>
+          <input type="text" />
+        </div>
+        <Button
+          label="Login"
+          buttonType="primary"
+          handleFunction={handleOnClick}
+        />
+      </div>
+    </div>
+  );
 }
